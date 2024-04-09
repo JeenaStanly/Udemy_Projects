ef200be62b5fd6e4dfabe9d66ffeeb204ea19e56
JeenaStanly <jeenastanly66@gmail.com>
2024-04-09 13:58:19 -0400

    Learned how to set a toggle button and toggle conditions

---
+++ CheckboxToggle/script.js
@@ -0,0 +1,29 @@
+const toggles = document.querySelectorAll('.toggle')
+const good = document.querySelector('#good')
+const cheap = document.querySelector('#cheap')
+const fast = document.querySelector('#fast')
+
+toggles.forEach(toggle => toggle.addEventListener('change', (e) => {
+    doTheTrick(e.target)
+}))
+
+function doTheTrick(theClickedOne) {
+    if (good.checked && cheap.checked) {
+        {
+            fast.checked = false
+        }
+
+    }
+    if (good.checked && fast.checked) {
+        {
+            cheap.checked = false
+        }
+
+    }
+    if (fast.checked && cheap.checked) {
+        {
+            good.checked = false
+        }
+
+    }
+}
